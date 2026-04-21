b88639d67852f2960df9b95c806d027f3cc61d8b
Abhijith Desai <abdesai@qti.qualcomm.com>
2026-04-21 13:45:19 +0530

    Update yocto-check-layer.sh script to find the correct path
    Signed-off-by: Abhijith Desai <abdesai@qti.qualcomm.com>

--- ci/yocto-check-layer.sh
+++ ci/yocto-check-layer.sh
@@ -38,6 +38,8 @@ CMD="$CMD --dependency $WORK_DIR/oe-core/meta"
 # Disable automatic testing of dependencies
 CMD="$CMD --no-auto-dependency"
 # Set machines to all machines defined in this BSP layer
-CMD="$CMD --machines $(echo $(find $WORK_DIR/meta-qcom/conf/machine/ -maxdepth 1 -name *.conf -exec basename {} .conf \; ))"
+if [ -d "$WORK_DIR/meta-qcom/conf/machine" ]; then
+    CMD="$CMD --machines $(echo $(find $WORK_DIR/meta-qcom/conf/machine/ -maxdepth 1 -name *.conf -exec basename {} .conf \; ))"
+fi
 
 exec $CMD
